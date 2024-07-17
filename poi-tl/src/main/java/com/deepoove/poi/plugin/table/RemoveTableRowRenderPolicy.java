a5ad72a19db85a8e72fa2621ec70099ff1c03c42
zhanqh <2855942332@qq.com>
2024-07-17 10:34:58 +0800

    删除表格行策略优化

--- poi-tl/src/main/java/com/deepoove/poi/plugin/table/RemoveTableRowRenderPolicy.java
+++ poi-tl/src/main/java/com/deepoove/poi/plugin/table/RemoveTableRowRenderPolicy.java
@@ -1,16 +1,17 @@
 package com.deepoove.poi.plugin.table;
 
 import com.deepoove.poi.XWPFTemplate;
-import com.deepoove.poi.config.Configure;
 import com.deepoove.poi.exception.RenderException;
 import com.deepoove.poi.policy.RenderPolicy;
-import com.deepoove.poi.resolver.TemplateResolver;
 import com.deepoove.poi.template.ElementTemplate;
 import com.deepoove.poi.template.MetaTemplate;
 import com.deepoove.poi.template.run.RunTemplate;
 import com.deepoove.poi.util.TableTools;
+import com.deepoove.poi.util.WordTableUtils;
 import org.apache.poi.xwpf.usermodel.*;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTc;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTcPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTVMerge;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -35,40 +36,71 @@ public class RemoveTableRowRenderPolicy implements RenderPolicy {
             if (data instanceof Boolean) {
                 Boolean d = (Boolean) data;
                 if (d) {
-                    // 获取这一行的所有的 标签 停止渲染
-                    List<MetaTemplate> elementTemplates = template.getElementTemplates();
-                    List<XWPFTableCell> tableCells = tableRow.getTableCells();
-                    Configure config = template.getConfig();
-                    TemplateResolver resolver = new TemplateResolver(template.getConfig().copy(config.getGramerPrefix(), config.getGramerSuffix()));
-                    List<MetaTemplate> newTemplates = new ArrayList<>();
-                    for (XWPFTableCell tableCell : tableCells) {
-                        List<MetaTemplate> templates = resolver.resolveBodyElements(tableCell.getBodyElements());
-                        newTemplates.addAll(templates);
-                    }
-                    template.setElementTemplates(removeElementTemplate(elementTemplates, newTemplates));
-                    for (int i = 0; i < tableRow.getTableCells().size(); i++) {
-                        XWPFTableCell templateCell = tableRow.getCell(i);
-                        // 获取是否跨行
-                        CTTcPr tcPr = templateCell.getCTTc().getTcPr();
-                        if (null != tcPr && null != tcPr.getGridSpan() && tcPr.getGridSpan().getVal().intValue() == 1) {
-                            tableRow.removeCell(i + 1);
-                        }
-                    }
+                    removeTableCellNoSpan(tableRow, rowIndex);
                 }
+            } else if (data != null) {
+                removeTableCellNoSpan(tableRow, rowIndex);
             }
         } catch (Exception e) {
             throw new RenderException("Remove line failure: " + e.getMessage(), e);
         }
     }
 
+    /**
+     * 删除表格行，跨行数据需要单独处理。
+     * 情况一：如果是跨行的开头则需要把数据弄到下一行对应的列，然后把他的跨行标记为 restart
+     * 情况二：TODO
+     *      如果是跨行的下一列也删除
+     *      如果是跨行的n行 都删除则不应该移动restart的数据，而是删除数据
+     *
+     * @param tableRow
+     */
+    private void removeTableCellNoSpan(XWPFTableRow tableRow, int rowIndex) {
+        int size = tableRow.getTableCells().size();
+        XWPFTable table = tableRow.getTable();
+        // 判断是否有某一列跨行，如果有则合并单元格到下一行
+        boolean isHasMergedVertically = false;
+        for (int i = size - 1; i > 0; i--) {
+            XWPFTableCell templateCell = tableRow.getCell(i);
+            // 获取是否跨行
+            Integer vMerge = getVMerge(templateCell);
+            if (vMerge != null && vMerge == 2){
+                // 获取跨行数
+                int mergedRows = WordTableUtils.getMergedRows(table, rowIndex, i);
+                WordTableUtils.moveCellToNextRow(table, rowIndex, i);
+                if (mergedRows == 2){
+                    // 把跨列的标记取消掉
+                    table.getRow(rowIndex+1).getTableCells().get(i).getCTTc().getTcPr().unsetVMerge();
+                }
+            }
+        }
+        tableRow.getTable().removeRow(rowIndex);
+    }
+
+    /**
+     * 获取跨行数据，restart=2 表示跨行的开始
+     * continue=1是跨行数据的持续，知道跨行信息不存在则结束跨行
+     *
+     * @param cell
+     * @return null则表示没有跨行
+     */
+    public Integer getVMerge(XWPFTableCell cell) {
+        // 获取单元格属性
+        CTTcPr tcPr = cell.getCTTc().getTcPr();
+        if (tcPr != null) {
+            // 获取垂直合并属性
+            CTVMerge vMerge = tcPr.getVMerge();
+            if (vMerge != null) {
+                // continue
+                return vMerge.getVal().intValue();
+            }
+        }
+        return null;
+    }
+
+
     private List<MetaTemplate> removeElementTemplate(List<MetaTemplate> oldElementTemplates, List<MetaTemplate> newTemplates) {
         HashSet<String> collect = newTemplates.stream().map(MetaTemplate::variable).collect(Collectors.toCollection(HashSet::new));
         return oldElementTemplates.stream().filter(e -> !collect.contains(e.variable())).collect(Collectors.toCollection(ArrayList::new));
     }
-
-    private int getRowIndex(XWPFTableRow row) {
-        List<XWPFTableRow> rows = row.getTable().getRows();
-        return rows.indexOf(row);
-    }
-
 }
---
+++ poi-tl/src/main/java/com/deepoove/poi/util/WordTableUtils.java
@@ -0,0 +1,184 @@
+package com.deepoove.poi.util;
+
+import org.apache.poi.xwpf.usermodel.*;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTc;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTcPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTVMerge;
+
+import java.util.List;
+
+public class WordTableUtils {
+
+    /**
+     * 获取表格的最大列数
+     *
+     * @param table XWPFTable 表格
+     * @return 最大列数
+     */
+    public static int getMaxColCount(XWPFTable table) {
+        int maxColCount = 0;
+        List<XWPFTableRow> rows = table.getRows();
+        for (XWPFTableRow row : rows) {
+            int colCount = row.getTableCells().size();
+            if (colCount > maxColCount) {
+                maxColCount = colCount;
+            }
+        }
+        return maxColCount;
+    }
+
+    /**
+     * 获取表格的最大实际列数（包括所有跨行）
+     *
+     * @param table XWPFTable 表格
+     * @return 最大列数
+     */
+    public static int getMaxActualCol(XWPFTable table) {
+        int max = -1;
+        for (XWPFTableRow row : table.getRows()) {
+            List<XWPFTableCell> tableCells = row.getTableCells();
+            int temp = 0;
+            for (XWPFTableCell cell : tableCells) {
+                if (cell.getCTTc() == null) {
+                    temp += 1;
+                    continue;
+                }
+                CTTc ctTc = cell.getCTTc();
+                if (ctTc.getTcPr() == null) {
+                    temp += 1;
+                    continue;
+                }
+                if (ctTc.getTcPr().getGridSpan() == null) {
+                    temp += 1;
+                    continue;
+                } else {
+                    temp += ctTc.getTcPr().getGridSpan().getVal().intValue();
+                }
+            }
+            if (temp > max) {
+                max = temp;
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 获取跨行数据，restart=2 表示跨行的开始
+     * continue=1是跨行数据的持续，知道跨行信息不存在则结束跨行
+     *
+     * @param cell 单元格
+     * @return null则表示没有跨行
+     */
+    public static Integer getVMerge(XWPFTableCell cell) {
+        // 获取单元格属性
+        CTTcPr tcPr = cell.getCTTc().getTcPr();
+        if (tcPr != null) {
+            // 获取垂直合并属性
+            CTVMerge vMerge = tcPr.getVMerge();
+            if (vMerge != null) {
+                return vMerge.getVal().intValue();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 将当前单元格移动到下一行同一列
+     *
+     * @param table    XWPFTable 表格
+     * @param rowIndex 当前行索引
+     * @param colIndex 当前列索引
+     */
+    public static void moveCellToNextRow(XWPFTable table, int rowIndex, int colIndex) {
+        XWPFTableRow currentRow = table.getRow(rowIndex);
+        XWPFTableCell currentCell = currentRow.getCell(colIndex);
+
+        XWPFTableRow nextRow = table.getRow(rowIndex + 1);
+        if (nextRow == null) {
+            return;
+        }
+
+        XWPFTableCell newCell = nextRow.getCell(colIndex);
+        if (newCell == null) {
+            newCell = nextRow.createCell();
+        }
+
+        copyVerticallyCellContent(currentCell, newCell);
+        clearCellContent(currentCell);
+    }
+
+    /**
+     * 复制跨列的单元格内容包括样式，由于跨列的数据只在第一一行有，所以不需要清除目标单元格的内容
+     *
+     * @param source source
+     * @param target target
+     */
+    public static void copyVerticallyCellContent(XWPFTableCell source, XWPFTableCell target) {
+        removeAllParagraphs(target);
+        for (XWPFParagraph paragraph : source.getParagraphs()) {
+            XWPFParagraph newParagraph = target.addParagraph();
+            copyParagraph(paragraph, newParagraph);
+        }
+        target.getCTTc().setTcPr(source.getCTTc().getTcPr());
+    }
+
+    // 复制段落内容
+    public static void copyParagraph(XWPFParagraph source, XWPFParagraph target) {
+        for (XWPFRun run : source.getRuns()) {
+            XWPFRun newRun = target.createRun();
+            newRun.setText(run.text());
+        }
+    }
+
+    // 清空单元格内容
+    public static void clearCellContent(XWPFTableCell cell) {
+        cell.removeParagraph(0);
+    }
+
+    /**
+     * 获取表格的跨列数（问题：如果列错位了处理方式就有问题）
+     *
+     * @param table    表格
+     * @param startRow 开始行
+     * @param colIndex 查找列
+     * @return 跨行数目，如果为0表示没有跨行
+     */
+    public static int getMergedRows(XWPFTable table, int startRow, int colIndex) {
+        int i = startRow + 1;
+        int size = table.getRows().size();
+        for (; i <= size; i++) {
+            if (table.getRow(i).getTableCells().get(colIndex) == null) {
+                break;
+            }
+            XWPFTableCell xwpfTableCell = table.getRow(i).getTableCells().get(colIndex);
+            if (xwpfTableCell.getCTTc() == null) {
+                break;
+            }
+            CTTc ctTc = xwpfTableCell.getCTTc();
+            if (ctTc.getTcPr() == null) {
+                break;
+            }
+            if (ctTc.getTcPr().getVMerge() == null) {
+                break;
+            }
+            if (ctTc.getTcPr().getVMerge().getVal().intValue() != 1) {
+                break;
+            }
+        }
+        return i - startRow;
+    }
+
+    /**
+     * 删除单元格中的所有段落
+     *
+     * @param cell XWPFTableCell 单元格
+     */
+    public static void removeAllParagraphs(XWPFTableCell cell) {
+        List<XWPFParagraph> paragraphs = cell.getParagraphs();
+        int size = paragraphs.size();
+        for (int i = size - 1; i >= 0; i--) {
+            cell.removeParagraph(i);
+        }
+    }
+
+}
