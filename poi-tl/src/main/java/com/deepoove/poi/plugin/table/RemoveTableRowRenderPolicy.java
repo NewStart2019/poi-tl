87129d3bc5a0813ae94ca8b55c60a67348c677a5
zhanqh <2855942332@qq.com>
2026-07-07 09:59:03 +0800

    表格删除行尾部补行功能添加

--- poi-tl/src/main/java/com/deepoove/poi/plugin/table/RemoveTableRowRenderPolicy.java
+++ poi-tl/src/main/java/com/deepoove/poi/plugin/table/RemoveTableRowRenderPolicy.java
@@ -54,31 +54,36 @@ public class RemoveTableRowRenderPolicy implements RenderPolicy {
             XWPFTableRow tableRow = tagCell.getTableRow();
             int rowIndex = WordTableUtils.findRowIndex(tableRow);
             // compute 为空 或 表达式为true 时 删除本行
+            boolean flag = false;
             if (compute == null || compute == defaultDeleteValue) {
                 removeTableCellNoSpan(tableRow, rowIndex);
+                flag = true;
             } else if (compute instanceof Boolean && Boolean.FALSE.equals(compute)) {
                 removeTableCellNoSpan(tableRow, rowIndex);
+                flag = true;
             }
 
-            String tagName =  "open_addRow";
-            String insertPosition = runTemplate.getTagName() + "_insertPosition";
-            // 如果想要在表格尾部添加一行，则 添加一个 xxxx_addRow 字段
-            if (globalEnv.get(tagName) != null) {
-                Object o = globalEnv.get(insertPosition);
-                XWPFTable table = tableRow.getTable();
-                int position = 0;
-                if (o == null) {
-                    // 在表格尾部补上一行
-                    position = table.getRows().size() - 1;
-                } else {
-                    // 在表格删除位置下面第几行补上一行
-                    int p = (int) o;
-                    position = rowIndex + p - 1;
+            if (flag) {
+                String tagName = "open_addRow";
+                String insertPosition = runTemplate.getTagName() + "_insertPosition";
+                // 如果想要在表格尾部添加一行，则 添加一个 xxxx_addRow 字段
+                if (globalEnv.get(tagName) != null) {
+                    Object o = globalEnv.get(insertPosition);
+                    XWPFTable table = tableRow.getTable();
+                    int position = 0;
+                    if (o == null) {
+                        // 在表格尾部补上一行
+                        position = table.getRows().size() - 1;
+                    } else {
+                        // 在表格删除位置下面第几行补上一行
+                        int p = (int) o;
+                        position = rowIndex + p - 1;
+                    }
+                    XWPFTableRow lastRow = table.getRow(position);
+                    XWPFTableRow newRow = table.insertNewTableRow(position);
+                    WordTableUtils.copyLineContent(lastRow, newRow, position);
+                    WordTableUtils.cleanRowTextContent(lastRow);
                 }
-                XWPFTableRow lastRow = table.getRow(position);
-                XWPFTableRow newRow = table.insertNewTableRow(position);
-                WordTableUtils.copyLineContent(lastRow, newRow, position);
-                WordTableUtils.cleanRowTextContent(lastRow);
             }
         } catch (Exception e) {
             throw new RenderException("Remove line failure: " + e.getMessage(), e);
